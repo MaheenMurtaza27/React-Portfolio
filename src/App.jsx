aefb0fc7300eb1974e529d786ef27dd4eeefc872
Maheen <maheensiyal27@gmail.com>
2026-04-25 02:35:08 +0500

    remove invalid characters

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,7 @@
 import { useState } from 'react'
 import './App.css'
- HEAD
-import Header from "./Components/Header";
-import Home from "./Components/Home";
-import About from "./Components/About";
-import Projects from "./Components/Projects";
-import Skills from "./Components/Skills";
-import Feedbacks from "./Components/Feedbacks";
-import Blogs from "./Components/Blogs";
-import Contact from "./Components/Contact";
 
- 8a198fb (fix component casing)
+
 
 import Header from "./components/header";
 import Home from "./components/home";
